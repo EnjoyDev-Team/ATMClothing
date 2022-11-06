6f88d4f9af61bf93dda47ae1ae617389dbd8f3a5
M2KV <wgmin.it@outlook.com>
2022-11-06 21:20:59 +0700

    feat: routes

--- frontend/customer/src/routes/index.js
+++ frontend/customer/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import PrivateRoute from './privateRoute';
 
 import Home from '../pages/home';
@@ -11,32 +11,48 @@ import Profile from '../pages/auth/Profile';
 
 const Navigation = () => {
   const authenticated = true;
-
   return (
-    <main>
-      <Routes>
-        {/* Auth */}
+    <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+
+        <Route path="/home" element={<Home />} />
+
+        {/* AUTH ROUTE */}
         <Route element={<PrivateRoute isAllowed={authenticated} redirectPath="/home" />}>
-          <Route path="/login" name="login" element={<Login />} />
-          <Route path="/register" name="register" element={<Register />} />
+            <Route path="/login" name="login" element={<Login />} />
+            <Route path="/register" name="register" element={<Register />} />
         </Route>
         <Route path="/forgot" name="forgot" element={<ForgotPassword />} />
 
         {/* Sale/Custom/Donate */}
         <Route path="/services">
-          <Route path="/sale" name="sale" element={<NotFound />} />
-          <Route path="/custom" name="custom" element={<NotFound />} />
-          <Route path="/donate" name="donate" element={<NotFound />} />
+            <Route path="sale" name="sale" element={<NotFound />} />
+            <Route path="custom" name="custom" element={<NotFound />} />
+            <Route path="donate" name="donate" element={<NotFound />} />
         </Route>
 
-        {/* Protected Routes */}
+        {/* PROTECTED ROUTE */}
         <Route element={<PrivateRoute isAllowed={authenticated} redirectPath="/login" />}>
-          <Route path="/profile" name="profile" element={<Profile />} />
+          {/* USER ROUTE */}
+            <Route path="/profile" name="profile" element={<Profile />} />
+        </Route>
+
+        {/* PRODUCT ROUTE */}
+        <Route path="/products">
+            <Route index element={<>Products Page</>} />
+            <Route path=":id" element={<>Sub-Products Page</>} />
+            <Route path="list/:page" element={<>Products Page X</>} />
+            <Route path="list/:category/:page" element={<>Products Page with category & page X</>} />
+        </Route>
+
+        {/* ShoppingCart + Payment ROUTE */}
+        <Route path="/shopping">
+            <Route index element={<>Shopping Cart Page</>} />
+            <Route path="payment" element={<>Payment Page</>} />
         </Route>
 
         <Route path="*" name="notFound" element={<NotFound />} />
-      </Routes>
-    </main>
+    </Routes>
   );
 };
 
