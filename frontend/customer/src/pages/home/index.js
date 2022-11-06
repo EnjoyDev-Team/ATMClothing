1e51eb0fe4e4e56ed0507871a57dbb9273969d8c
M2KV <wgmin.it@outlook.com>
2022-11-06 21:10:26 +0700

    feat: details component

---
+++ frontend/customer/src/pages/home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Sell from '../sell/Sell';
+
+import classes from './styles.module.scss';
+
+const Home = () => {
+  const photo = useSelector((state) => state.photos);
+  console.log(photo);
+
+  const HelloWorld = () => console.log('hello world');
+
+  return (
+    <div onClick={HelloWorld}>
+      {/* <Sell /> */}
+    </div>
+  );
+};
+
+export default Home;
