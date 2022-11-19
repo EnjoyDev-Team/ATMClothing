837f517cc4c916a5f0abdc8c9224b1665f81e256
M2KV <wgmin.it@outlook.com>
2022-11-19 17:49:54 +0700

    refactor: add error message to product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,63 +3,63 @@ const mongoose = require('mongoose');
 const productModel = new mongoose.Schema({
     category: {
         type: String,
-        required: true,
+        required: [true, "Category can not is empty!"],
     },
     slug: {
         type: String,
-        required: true,
+        required: [true, "Slug can not is empty!"],
     },
     name: {
         type: String,
-        required: true,
+        required: [true, "Name of product can not is empty!"],
     },
     price: {
         type: String,
-        required: true,
+        required: [true, "Price can not is empty!"],
     },
     sale: {
         type: String,
-        required: true,
+        required: [true, "Sale can not is empty!"],
     },
     size: {
         type: String,
-        required: true,
+        required: [true, "Size can not is empty!"],
     },
     note_size: String,
     color: {
         type: String,
-        required: true,
+        required: [true, "Color can not is empty!"],
     },
     amount: {
         type: Number,
-        required: true,
+        required: [true, "Amount can not is empty!"],
     },
     material: String,
     facility: [
         {
             code: {
                 type: String,
-                required: true,
+                required: [true, "Code of facility can not is empty!"],
             },
             name: {
                 type: String,
-                required: true,
+                required: [true, "Name of facility can not is empty!"],
             },
             address: {
                 type: String,
-                required: true,
+                required: [true, "Address of facility can not is empty!"],
             },
             _id: false,
         },
     ],
     img: {
         type: String,
-        required: true,
+        required: [true, "Image of product can not is empty!"],
     },
     other_img: [String],
     status: {
         type: String,
-        required: true,
+        required: [true, "Status of product can not is empty!"],
     },
     description: String,
     create_at: {
