15fd16019ca75468460a53ae16ad75f56cb0f994
PhongTran1804 <trannguyenphongg@gmail.com>
2022-11-23 10:32:45 +0700

    detail_request page first part

--- frontend/customer/src/routes/index.js
+++ frontend/customer/src/routes/index.js
@@ -19,6 +19,7 @@ import Shopping from '../pages/shopping/Shopping';
 import ProductLayout from '../layouts/ProductLayout';
 import Product from '../pages/Products/Product/Product';
 import auth from '../utils/auth';
+import Detail from '../pages/detail_request/Detail';
 
 const Navigation = () => {
     const authenticated = !!auth.getAccessToken();
@@ -36,22 +37,23 @@ const Navigation = () => {
             <Route element={<Layout />}>
                 <Route path="/home" element={<Home />} />
 
-                {/* Sale/Custom/Donate */}
-                <Route path="/services">
-                    <Route index element={<Services />} />
-                    <Route path="sale" name="sale" element={<Sell />} />
-                    <Route path="custom" name="custom" element={<Custom />} />
-                    <Route path="donate" name="donate" element={<Donate />} />
-                </Route>
-
                 {/* PROTECTED ROUTE */}
                 <Route element={<PrivateRoute isAllowed={authenticated} redirectPath="/login" />}>
+                    {/* Sale/Custom/Donate */}
+                    <Route path="/services">
+                        <Route index element={<Services />} />
+                        <Route path="sale" name="sale" element={<Sell />} />
+                        <Route path="custom" name="custom" element={<Custom />} />
+                        <Route path="donate" name="donate" element={<Donate />} />
+                    </Route>
+
                     {/* USER ROUTE */}
                     <Route path="/profile" name="profile" element={<Profile />} />
+                    <Route path="/detailOrder" name="detailOrder" element={<Detail />} />
                 </Route>
 
-                {/* PRODUCT ROUTE */}
-                <Route path="/products">
+                 {/* PRODUCT ROUTE */}
+                 <Route path="/products">
                     <Route element={<ProductLayout />}>
                         <Route index element={<Product />} />
                         <Route path="search/:name" element={<Product />} />
@@ -59,13 +61,14 @@ const Navigation = () => {
                     <Route path=":id" element={<ProductDetail />} />
                     <Route path="list/:page" element={<>Products Page X</>} />
                     <Route path="list/:category/:page" element={<>Products Page with category & page X</>} />
-                </Route>
+                 </Route>
 
                 {/* ShoppingCart + Payment ROUTE */}
                 <Route path="/shopping">
                     <Route index element={<Shopping />} />
                     <Route path="payment" element={<>Payment Page</>} />
                 </Route>
+
                 <Route path="*" name="notFound" element={<NotFound />} />
             </Route>
         </Routes>
