0352267814aa27cd688f0f7b9a27a81db7c10508
M2KV <wgmin.it@outlook.com>
2022-11-28 20:04:16 +0700

    feat: sell services

--- frontend/customer/src/components/Services/ServiceDetails/ServiceDetails.js
+++ frontend/customer/src/components/Services/ServiceDetails/ServiceDetails.js
@@ -1,61 +1,150 @@
-import React from 'react';
+/* eslint-disable react/forbid-prop-types */
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import BoxWrapper from '../../BoxWrapper/BoxWrapper';
 import ButtonCT from '../../ButtonCT/ButtonCT';
-import DetailCard from '../../DetailCard/DetailCard';
 import classes from './ServiceDetails.module.scss';
+import { axiosClient } from '../../../api/axios';
 
-const productDetails = [
-  {
-    name: 'Đầm dạ hội dành cho nữ',
-    title: ['Danh mục', 'Tình trạng', 'Số lượng'],
-    content: ['Thời trang nữ', 'Tốt', '2'],
-    price: '80.000đ',
-  },
-  {
-    name: 'Đầm dạ hội dành cho nữ',
-    title: ['Danh mục', 'Tình trạng', 'Số lượng'],
-    content: ['Thời trang nữ', 'Tốt', '2'],
-    price: '80.000đ',
-  },
-  {
-    name: 'Đầm dạ hội dành cho nữ',
-    title: ['Danh mục', 'Tình trạng', 'Số lượng'],
-    content: ['Thời trang nữ', 'Tốt', '2'],
-    price: '80.000đ',
-  },
-];
+const ServiceDetails = ({ button, productDetails, screens }) => {
+  const [step, setStep] = useState(0);
+  const paymentData = useRef('');
+  const [payment, setPayment] = useState(false);
+  const [isPayment, setIsPayment] = useState(false);
 
-const ServiceDetails = () => (
-    <div className={classes.main}>
-        <BoxWrapper heading="Bán sản phẩm" width="69.5rem" height="41.3rem">
-            <>
-                {productDetails.map((el, index) => {
-                  const keys = `${el.name}_${index}`;
-                  return <DetailCard key={keys} details={el} />;
-                })}
-            </>
-        </BoxWrapper>
-        <BoxWrapper heading="Chi tiết hóa đơn" width="40.7rem">
-            <>
-                <div className={classes.order__content}>
-                    <div className={classes['order__content-item']}>
-                        <div>Tổng số lượng sản phẩm</div>
-                        <div>Phương thức vận chuyển</div>
-                        <div>Tổng tiền dự kiến</div>
+  useEffect(() => {
+    const total = productDetails && productDetails.length
+      ? productDetails.reduce((prev, cur) => prev + +cur.price.replace(/[^0-9]+/g, ''), 0)
+      : 0;
+
+    paymentData.current = {
+      products: productDetails,
+      status: 'Đang kiểm tra',
+      service: 'Sell',
+      totalPrice: `${`${total}`.replace(/\B(?=(\d{3})+(?!\d))/g, '.')}đ`,
+    };
+  }, [productDetails]);
+
+  const CancelPayment = () => {
+    paymentData.current = { products: productDetails };
+
+    setPayment(false);
+    setStep(0);
+  };
+
+  const goBack = () => setStep((prev) => (prev > 0 ? prev - 1 : prev));
+
+  const handleNext = (event) => {
+    event.preventDefault();
+    if (!paymentData.current || !paymentData.current.products || !paymentData.current.products.length) return;
+
+    if (payment === true) {
+      // send a payment
+      console.log(paymentData.current);
+      setIsPayment(true);
+      axiosClient
+        .post('/services', { paymentData: { ...paymentData.current, uid: '123acb123ns3' } })
+        .then((res) => {
+          console.log(res.data);
+          setIsPayment(false);
+          setPayment(false);
+          setStep(0);
+        })
+        .catch((err) => console.log(err));
+      return;
+    }
+
+    setStep((prev) => (prev < screens.length - 1 ? prev + 1 : prev));
+  };
+
+  const onChange = (data) => {
+    if (data) {
+      paymentData.current = { ...paymentData.current, ...data };
+    }
+  };
+
+  const Screen = screens[step];
+
+  const buttons = step === 0 ? (
+    button
+  ) : (
+      // <button
+      //   type="button"
+      //   onClick={() => setStep((prev) => (prev > 0 ? prev - 1 : prev))}
+      //   className={classes.back__btn}
+      // >
+      //     <span>Go back</span>
+      //     <FontAwesomeIcon icon={faArrowLeft} />
+      // </button>
+      <> </>
+  );
+
+  const maxHeight = step === 0 ? '41.3rem' : '';
+  const minHeight = step === 2 ? '28.8rem' : '';
+
+  useEffect(() => {
+    console.log(paymentData.current);
+  });
+
+  return (
+        <form className={classes.main} onSubmit={handleNext}>
+            <BoxWrapper
+              heading="Bán sản phẩm"
+              width="69.5rem"
+              maxHeight={maxHeight}
+              minHeight={minHeight}
+              button={buttons}
+            >
+                <Screen productDetails={productDetails} onChange={onChange} setPayment={setPayment} />
+            </BoxWrapper>
+            <BoxWrapper heading="Chi tiết hóa đơn" width="40.7rem">
+                <>
+                    <div className={classes.order__content}>
+                        <div className={classes['order__content-item']}>
+                            <div>Tổng số lượng sản phẩm</div>
+                            <div>Phương thức vận chuyển</div>
+                            <div>Tổng tiền dự kiến</div>
+                        </div>
+                        <div className={classes['order__content-item']}>
+                            <div>2</div>
+                            <div>2</div>
+                            <div>130.000đ</div>
+                        </div>
                     </div>
-                    <div className={classes['order__content-item']}>
-                        <div>2</div>
-                        <div>2</div>
-                        <div>130.000đ</div>
+                    <div className={classes.order__content__controls}>
+                        <ButtonCT
+                          onClick={CancelPayment}
+                          type="button"
+                          redLinear
+                          content="Hủy"
+                          disabled={isPayment === true}
+                        />
+                        <ButtonCT type="submit" greenLinear content="Thanh toán" />
                     </div>
-                </div>
-                <div className={classes.order__content__controls}>
-                    <ButtonCT type="button" redLinear content="Hủy" />
-                    <ButtonCT type="button" greenLinear content="Thanh toán" />
-                </div>
-            </>
-        </BoxWrapper>
-    </div>
-);
+                </>
+            </BoxWrapper>
+        </form>
+  );
+};
+
+ServiceDetails.propTypes = {
+  button: PropTypes.element,
+  screens: PropTypes.arrayOf(PropTypes.elementType).isRequired,
+  productDetails: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      title: PropTypes.arrayOf(PropTypes.string).isRequired,
+      content: PropTypes.any.isRequired,
+      price: PropTypes.string,
+    })
+  ),
+};
+
+ServiceDetails.defaultProps = {
+  button: undefined,
+  productDetails: [],
+};
 
 export default ServiceDetails;
--- frontend/customer/src/pages/Service/Sell.js
+++ frontend/customer/src/pages/Service/Sell.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Sell.module.scss';
 import Form from '../../components/Services/Form/Form';
 import ServiceDetails from '../../components/Services/ServiceDetails/ServiceDetails';
@@ -6,15 +6,34 @@ import SellFormStep1 from './screens/Sale/SellFormStep1';
 import OptionalDetails from './screens/OptionalDetails';
 import FormStep3 from './screens/FormStep3';
 import FormStepFinal from './screens/FormStepFinal';
+import ButtonCT from '../../components/ButtonCT/ButtonCT';
+import addBtn from '../../assets/imgs/service/add-btn.png';
+import useAxios from '../../hooks/useAxios';
+import Details from './screens/Details';
+import PaymentDelivery from './screens/PaymentDelivery';
+import PaymentMethod from './screens/PaymentMethod';
 
 const Sell = () => {
-  const [step, setStep] = useState(0);
-  const screens = [
-    <SellFormStep1 setStep={setStep} />,
-    <OptionalDetails setStep={setStep} />,
-    <FormStep3 setStep={setStep} />,
-    <FormStepFinal success />,
-  ];
+  const screens = [SellFormStep1, OptionalDetails, FormStep3, FormStepFinal];
+
+  const [addNewProduct, setAddNewProduct] = useState(false);
+
+  const [product, setProduct] = useState([]);
+
+  const [response, error, isLoading] = useAxios('get', '/services/sell', {}, {}, []);
+
+  useEffect(() => {
+    if (!isLoading && !error && response.data) {
+      const newproductList = response.data.products.map((el) => ({
+        ...el,
+        content: [el.category, el.status, el.amount],
+        title: ['Danh mục', 'Tình trạng', 'Số lượng']
+      }));
+      setProduct(prev => [...prev, ...newproductList]);
+    }
+  }, [isLoading]);
+
+  const detailsScreen = [Details, PaymentDelivery, PaymentMethod];
 
   return (
         <div className={classes.body}>
@@ -22,12 +41,32 @@ const Sell = () => {
                 <h1>Bạn có quần áo, phụ kiện cũ không sử dụng</h1>
                 <p>Hãy tái tạo nó thông qua hệ thống ATM CLOTHING của chúng tôi...</p>
             </div>
-            <ServiceDetails />
-
-            <div className={classes.backdrop} />
-            <div className={classes.container}>
-                <Form title="Bán sản phẩm" step={step} screens={screens} />
-            </div>
+            <ServiceDetails
+              screens={detailsScreen}
+              productDetails={product}
+              button={(
+                    <ButtonCT className={classes.addbtn} onClick={() => setAddNewProduct(true)}>
+                        <div className={classes.addbtn__content}>
+                            <span>Thêm sản phẩm</span>
+                            <img width={27} src={addBtn} alt="add" />
+                        </div>
+                    </ButtonCT>
+                  )}
+            />
+            {addNewProduct && (
+                <>
+                    <div className={classes.backdrop} />
+                    <div className={classes.container}>
+                        <Form
+                          close={() => setAddNewProduct(false)}
+                          title="Bán sản phẩm"
+                          screens={screens}
+                          setProduct={setProduct}
+                          service="sell"
+                        />
+                    </div>
+                </>
+            )}
         </div>
   );
 };
