b54685e676aa3b69146433d345afc8f8829219a6
tranhuuchinh0902 <tranhuuchinh500@gmail.com>
2022-12-08 22:53:14 +0700

    update Products Page

--- frontend/admin/src/routes/index.js
+++ frontend/admin/src/routes/index.js
@@ -7,6 +7,9 @@ import PrivateRoute from './privateRoute';
 import Login from '../pages/auth';
 import Home from '../pages/home';
 import NotFound from '../pages/notFound';
+import Products from '../pages/Products/Products';
+import EditProduct from '../pages/EditProduct/EditProduct';
+import AddProduct from '../pages/AddProduct/AddProduct';
 import User from '../pages/users/User';
 import EditUser from './../pages/users/EditUser';
 import AddUser from './../pages/users/AddUser';
@@ -23,6 +26,9 @@ const Navigation = () => {
                 <Route path="/users" element={<User />} />
                 <Route path="/users/editUser" element={<EditUser />} />
                 <Route path="/users/addUser" element={<AddUser />} />
+                <Route path="/products" name="products" element={<Products />} />
+                <Route path="/edit" name="products" element={<EditProduct />} />
+                <Route path="/add_product" name="products" element={<AddProduct />} />
                 <Route path="*" name="notFound" element={<NotFound />} />
             </Routes>
         </main>
