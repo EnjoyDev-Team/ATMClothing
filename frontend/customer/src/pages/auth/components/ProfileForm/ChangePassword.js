d480abab62a49e2dcd7daebe203300e7a0858f14
M2KV <wgmin.it@outlook.com>
2022-12-12 21:00:27 +0700

    feat: create user profile

--- frontend/customer/src/pages/auth/components/ProfileForm/ChangePassword.js
+++ frontend/customer/src/pages/auth/components/ProfileForm/ChangePassword.js
@@ -1,31 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ButtonCT from '../../../../components/ButtonCT/ButtonCT';
 import classes from './ProfileForm.module.scss';
+import auth from '../../../../utils/auth';
+import useAxiosPrivate from '../../../../hooks/useAxiosPrivate';
 
 const ChangePassword = () => {
-  const a = 0;
+  const [userPassword, setUserPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const axiosPrivate = useAxiosPrivate();
+
+  const UpdateUserPassword = (event) => {
+    event.preventDefault();
+
+    if (!userPassword.password && userPassword.password !== userPassword.passwordConfirm) {
+      setError({
+        loading: false,
+        error: 'Số điện thoại hoặc mật khẩu không đúng. Vui lòng thử lại!',
+      });
+    }
+
+    axiosPrivate
+      .patch('/users/password', {
+        user: {
+          ...userPassword,
+        },
+      })
+      .then((res) => {
+        auth.login(res.data);
+        setSuccess(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError('Số điện thoại hoặc mật khẩu không đúng. Vui lòng thử lại!');
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setError(false);
+          setSuccess(false);
+        }, 3000);
+      });
+  };
+
+  const updatePassword = (event) => {
+    setUserPassword((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
-    <>
-      <ul className={classes.profileForm}>
-        <li className={classes.profileForm__item}>
-          <p className={classes['profileForm__item-title']}>Mật khẩu cũ</p>
-          <input type="password" placeholder="Nhập mật khẩu cũ" />
-        </li>
-        <Link className={classes.forgotPW} to="/forgot">Quên mật khẩu</Link>
-        <li className={classes.profileForm__item}>
-          <p className={classes['profileForm__item-title']}>Mật khẩu mới</p>
-          <input type="password" placeholder="Nhập mật khẩu mới" />
-        </li>
-        <li className={classes.profileForm__item}>
-          <p className={classes['profileForm__item-title']}>Xác nhận mật khẩu</p>
-          <input type="password" placeholder="Nhập lại mật khẩu mới" />
-        </li>
-      </ul>
-      <ButtonCT primary medium className={classes.btn}>
-        Lưu thay đổi
-      </ButtonCT>
-    </>
+        <form onSubmit={UpdateUserPassword} action="PATCH">
+            { success && <div className={classes.success}>Cập nhật thành công</div> }
+            { error && <div className={classes.error}>{error}</div> }
+
+            <ul className={classes.profileForm}>
+                <li className={classes.profileForm__item}>
+                    <p className={classes['profileForm__item-title']}>Mật khẩu cũ</p>
+                    <input
+                      type="password"
+                      name="passwordCurrent"
+                      placeholder="Nhập mật khẩu cũ"
+                      onChange={updatePassword}
+                    />
+                </li>
+                <Link className={classes.forgotPW} to="/forgot">
+                    Quên mật khẩu
+                </Link>
+                <li className={classes.profileForm__item}>
+                    <p className={classes['profileForm__item-title']}>Mật khẩu mới</p>
+                    <input type="password" name="password" placeholder="Nhập mật khẩu mới" onChange={updatePassword} />
+                </li>
+                <li className={classes.profileForm__item}>
+                    <p className={classes['profileForm__item-title']}>Xác nhận mật khẩu</p>
+                    <input
+                      type="password"
+                      name="passwordConfirm"
+                      placeholder="Nhập lại mật khẩu mới"
+                      onChange={updatePassword}
+                    />
+                </li>
+            </ul>
+            <ButtonCT primary medium className={classes.btn}>
+                Lưu thay đổi
+            </ButtonCT>
+        </form>
   );
 };
 
--- frontend/customer/src/utils/auth.js
+++ frontend/customer/src/utils/auth.js
@@ -2,9 +2,13 @@ const { localStorage } = global.window;
 
 const auth = {
   login(data) {
+    localStorage.name = data.data.user.name;
+    localStorage.gender = data.data.user.gender;
+    localStorage.dob = data.data.user.dob;
     localStorage.phone = data.data.user.phone;
     localStorage.role = data.data.user.role;
     localStorage._id = data.data.user._id;
+    localStorage.avatar = data.data.user.avatar;
     localStorage.accessToken = data.access_token;
   },
 
@@ -26,6 +30,36 @@ const auth = {
 
   role() {
     return localStorage.role;
+  },
+
+  getInfo() {
+    return {
+      name: localStorage.name || '',
+      gender: localStorage.gender || '',
+      dob: localStorage.dob || '',
+      phone: `${localStorage.phone.substring(0, 2)}*****${localStorage.phone.substring(
+        localStorage.phone.length - 3,
+        localStorage.phone.length
+      )}`,
+      email: localStorage.email || '',
+    };
+  },
+
+  getAvatar() {
+    return !localStorage.avatar || localStorage.avatar === 'undefined' || localStorage.avatar === 'null'
+      ? ''
+      : localStorage.avatar;
+  },
+
+  updateAvatar(avt) {
+    localStorage.avatar = avt;
+  },
+
+  updateInfo(userInfo) {
+    localStorage.name = userInfo.name;
+    localStorage.gender = userInfo.gender;
+    localStorage.dob = userInfo.dob;
+    localStorage.email = userInfo.email;
   },
 
   logout() {
