ef9e0c845d7c2dcacafa81a7be4e89a4c0ab3ada
PhongTran1804 <trannguyenphongg@gmail.com>
2022-12-30 20:46:53 +0700

    users page fetch api

--- frontend/admin/src/components/users/ListUser.js
+++ frontend/admin/src/components/users/ListUser.js
@@ -1,11 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+/* eslint-disable import/no-useless-path-segments */
 /* eslint-disable indent */
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from './ListUser.module.scss';
 import addImg from '../../assets/imgs/users/Add.png';
 import UserItem from './UserItem';
+import { axiosClient } from '../../api/axios';
+import useAxios from './../../hooks/useAxios';
 
-const ListUser = () => {
+const ListUser = ({ setUserInformation }) => {
+    const [respond, error, isloading] = useAxios('get', '/users', {}, {}, []);
+    console.log(respond);
     const a = 0;
     return (
         <main className={classes.list__main}>
@@ -22,7 +30,7 @@ const ListUser = () => {
                     <input type="text" placeholder="Search" />
                 </div>
                 <div className={classes.list}>
-                    <UserItem />
+                    <UserItem listItem={respond.data} setUserInformation={setUserInformation} />
                 </div>
             </div>
         </main>
