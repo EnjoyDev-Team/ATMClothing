80ae2aeb308962600075806d24eac594c13362a0
M2KV <wgmin.it@outlook.com>
2023-01-03 00:37:14 +0700

    update images update product

--- backend/utils/spiltBase64.js
+++ backend/utils/spiltBase64.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 module.exports = (image) => {
-    const ext = image.split(';')[0].split('/')[1].replace('jpeg', 'jpg');
+    const ext = (image.split(';')[0].split('/')[1]) ? image.split(';')[0].split('/')[1].replace('jpeg', 'jpg')
+                                                    : image.split(';')[0].split(':')[1].replace('jpeg', 'jpg')
     const base64Data = image.split(';')[1].replace('base64,', '');
 
     return { ext, base64Data };
